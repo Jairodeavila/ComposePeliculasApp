fa4197797758293297cc5d307dee559f08a1645b
jairo <jairodeavila2004@gmail.com>
2025-01-24 20:51:25 -0500

    se agrego la capa de data

--- app/src/main/java/com/example/composepeliculasapp/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/composepeliculasapp/data/network/RetrofitClient.kt
@@ -1,19 +1,20 @@
 package com.example.composepeliculasapp.data.network
 
+import com.example.composepeliculasapp.utils.BASE_URL
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://api.themoviedb.org/3/"
-
-    private fun getRetrofit(): Retrofit {
+    private fun getRetrofit(): Retrofit{
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
+
     fun getApiService(): ApiService{
         return getRetrofit().create(ApiService::class.java)
     }
 }
+
 
